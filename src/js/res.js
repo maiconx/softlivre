f8b8b92fffb968f3111d1231c597a8fe65ba5c95
Maicon X <xsmaicon@gmail.com>
2016-09-14 05:41:28 -0300

    Destaque

--- src/js/res.js
+++ src/js/res.js
@@ -1,3 +1,20 @@
+var slideIndex = 0;
+carousel();
+
+   function carousel() {
+
+       var i;
+       var x = document.getElementsByClassName("mySlides");
+       for (i = 0; i < x.length; i++) {
+         x[i].style.display = "none";
+       }
+       slideIndex++;
+       if (slideIndex > x.length) {slideIndex = 1}
+       x[slideIndex-1].style.display = "block";
+       setTimeout(carousel, 2000);
+   }
+
+
    function toggle_visibility(id) {
       var e = document.getElementById(id);
       if(e.style.display == 'block')
